cd1202d3ee27cf845517641c070b66d1913e82a5
xuejian.sun <xuejian.sun@sumscope.com>
2018-09-26 17:44:52 +0800

    initialize project

---
+++ src/main/java/alibaba/dubbo/learn/analyze/DubboAnalyzeApplication.java
@@ -0,0 +1,12 @@
+package alibaba.dubbo.learn.analyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboAnalyzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboAnalyzeApplication.class, args);
+    }
+}
